b1bb17d804bfac58030af2dd5a3bf53d1fcc502b
amazinglySK <shashwat07ktm@gmail.com>
2023-05-20 21:32:02 +0400

    Added an extra condition

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,8 @@ import { verify } from "jsonwebtoken";
 
 export const requireAuth = (role) => {
     return (req, res, next) => {
-        const token = req.cookies.token || req.body.token || req.headers['access-token'];
+        const token =
+            req.cookies.token || req.body.token || req.headers["access-token"];
         if (!token) {
             res.status(401).json({
                 msg: "You need to be logged in to execute this",
@@ -10,7 +11,7 @@ export const requireAuth = (role) => {
             return;
         }
         const token_data = verify(token, process.env.JWT_SECRET);
-        if (!token_data || role !== token_data.role) {
+        if (!token_data || (role && role !== token_data.role)) {
             res.status(401).json({
                 msg: "You do not have the permission to get this data",
             });
